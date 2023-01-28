90a3737495f6ad0e892c4dc7d424b311103fc0a8
Toni Ismail <92683479+toniismail12@users.noreply.github.com>
2023-01-28 14:09:29 +0700

    Create button.js

---
+++ button.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Button = ({text, btn}) => {
+
+    if (btn == 'primary') {
+        return (
+            
+            <button className='bg-blue-pusri text-white hover:text-white p-2 rounded-lg hover:font-bold transition-all'> <span className="mx-3"> {text}  </span> </button>
+            
+        );
+    }
+
+    if (btn == 'success') {
+        return (
+            
+            <button className='bg-green-600 text-white hover:text-white p-2 rounded-lg hover:font-bold transition-all'> <span className="mx-3"> {text}  </span> </button>
+            
+        );
+    }
+
+    if (btn == 'warning') {
+        return (
+            
+            <button className='bg-yellow-500 text-white hover:text-white p-2 rounded-lg hover:font-bold transition-all'> <span className="mx-3 text-gray-900"> {text}  </span> </button>
+            
+        );
+    }
+
+    if (btn == 'danger') {
+        return (
+            
+            <button className='bg-red-500 text-white hover:text-white p-2 rounded-lg hover:font-bold transition-all'> <span className="mx-3 text-white"> {text}  </span> </button>
+            
+        );
+    }
+
+    if (btn == 'secondary') {
+        return (
+            
+            <button className='bg-gray-500 text-white hover:text-white p-2 rounded-lg hover:font-bold transition-all'> <span className="mx-3 text-white"> {text}  </span> </button>
+
+        );
+    }
+
+}
+
+export default Button
